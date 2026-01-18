5d2a1d93552935b5501e1459487a05de32dd3019
Ahmed Hrabi <hrabi.ahmed88@gmail.com>
2026-01-18 01:19:09 +0100

    update revalidate strategy

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -3,7 +3,7 @@ import { ProjectsGrid } from "@/features/projects/components/ProjectGrid";
 import { ProjectsHero } from "@/features/projects/components/ProjectHero";
 import { fetchProjects } from "@/features/projects/services/fetch-projects";
 
-export const revalidate = 3600;
+export const revalidate = 60;
 
 export default async function ProjectsPage() {
   const projects = await fetchProjects();
