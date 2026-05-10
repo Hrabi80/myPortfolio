6a9b3c0f49318773a9e88ce540f32b27a184a37b
Ahmed Hrabi <hrabi.ahmed88@gmail.com>
2026-05-10 23:07:37 +0100

    fix: ordering project

--- src/utils/sorting.ts
+++ src/utils/sorting.ts
@@ -1,7 +1,20 @@
-export const sortByPublishedAt = <T extends { publishedAt?: string }>(items: T[]) => {
+type ProjectSortItem = {
+  order?: number;
+  publishedAt: string;
+};
+
+const unorderedProjectPosition = Number.MAX_SAFE_INTEGER;
+
+const compareByPublishedAtDesc = <T extends ProjectSortItem>(a: T, b: T) => {
+  return new Date(b.publishedAt).getTime() - new Date(a.publishedAt).getTime();
+};
+
+export const sortByProjectOrder = <T extends ProjectSortItem>(items: T[]) => {
   return [...items].sort((a, b) => {
-    const aTime = a.publishedAt ? new Date(a.publishedAt).getTime() : 0;
-    const bTime = b.publishedAt ? new Date(b.publishedAt).getTime() : 0;
-    return bTime - aTime;
+    const orderDifference =
+      (a.order ?? unorderedProjectPosition) -
+      (b.order ?? unorderedProjectPosition);
+
+    return orderDifference || compareByPublishedAtDesc(a, b);
   });
 };
